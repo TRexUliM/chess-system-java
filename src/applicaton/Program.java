ae76306b600b6441fe9bf34992c31b6f66c35130
TRexUliM <trexulim@gmail.com>
2020-06-03 15:26:37 -0300

    Classe Position

--- src/applicaton/Program.java
+++ src/applicaton/Program.java
@@ -1,11 +1,13 @@
 package applicaton;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Oi Mundão véi sem portera!");
-
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 
 }
