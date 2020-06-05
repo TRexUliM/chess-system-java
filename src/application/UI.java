2a9d3cc002aedb73c1093d40b708ad2df2e7ceae
TRexUliM <trexulim@gmail.com>
2020-06-05 11:57:58 -0300

    Printing possible moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -30,6 +30,8 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
+	public static final String ANSI_BLINK = "\u001B[5m";
+	public static final String ANSI_RAPID_BLINK = "\u001B[6m";
 	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
@@ -57,7 +59,23 @@ public class UI {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print(pieces.length  - i + " |");
 			for (int j = 0; j < pieces.length; j++) {
-				printPiece(pieces[i][j], ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)));
+				printPiece(pieces[i][j], ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)), false);
+			}
+			System.out.println();
+			System.out.println(boardBorder);
+		}
+		System.out.println("    a   b   c   d   e   f   g   h");
+	}
+
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		clearScreen();
+		String boardBorder = "  +---+---+---+---+---+---+---+---+";
+		
+		System.out.println(boardBorder);
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print(pieces.length  - i + " |");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)), possibleMoves[i][j]);
 			}
 			System.out.println();
 			System.out.println(boardBorder);
@@ -65,26 +83,44 @@ public class UI {
 		System.out.println("    a   b   c   d   e   f   g   h");
 	}
 	
-	private static void printPiece(ChessPiece piece, Boolean fillBackground) {
+	
+	private static void printPiece(ChessPiece piece, Boolean fillBackground, Boolean showPossibleMove) {
 		String printOut = ""; 
+
+		if (showPossibleMove) {
+			System.out.print(ANSI_BLINK);
+		}
+
 		if (fillBackground)	{
 			printOut += ANSI_WHITE_BACKGROUND;
 		}
-		printOut += " ";
+		
+		if (showPossibleMove) {
+			printOut += "(";
+		} else {
+			printOut += " ";
+		}
 		
 		if (piece == null) {
-			printOut += ANSI_GREEN + "-";
+			printOut += ANSI_GREEN + "-" + ANSI_RESET;
 		} else {
             if (piece.getColor() == Color.WHITE) {
-            	printOut += ANSI_RED + piece;
+            	printOut += ANSI_RED + piece + ANSI_RESET;
             }
             else {
-            	printOut += ANSI_BLUE + piece;
+            	printOut += ANSI_BLUE + piece + ANSI_RESET;
             }
 		}
-		System.out.print( printOut + " " + ANSI_RESET + "|");
+		
+		if (fillBackground)	{
+			printOut += ANSI_WHITE_BACKGROUND;
+		}
+		
+		if (showPossibleMove) {
+			printOut += ANSI_BLINK + ")";
+		} else {
+			printOut += " ";
+		}
+		System.out.print( printOut + ANSI_RESET + "|");
 	}
-	
-	
-	
 }
